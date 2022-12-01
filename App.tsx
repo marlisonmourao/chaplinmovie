c9fa742e721a852fa5fbc759351bd3fcf3b61030
Angeluz <esteva0.silva015@gmail.com>
2022-11-30 23:53:13 -0400

    Alterando rota para a screen home

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
 import { LoadingIndicator } from '@components/Loading/styles';
 import { View } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
-
+import Home from '@screens/home/home';
 import theme from './src/theme';
 
 export default function App() {
   return (
 
     <ThemeProvider theme={theme}>
-      <View>
-        <LoadingIndicator />
-      </View>
+        <Home/>
     </ThemeProvider>
   );
 }
