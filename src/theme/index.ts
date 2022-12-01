0b8a606696e1611363e8830cc1f4482e6289da14
Angeluz <esteva0.silva015@gmail.com>
2022-11-30 23:55:42 -0400

    Adicionando novas cores

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,10 +1,12 @@
 export default {
   colors: {
+    white:'#fff',
     gray_500: '#5A6A83',
     gray_300: '#D1D5DB',
-    gray_800: '#374151',
+    gray_800: '#1F2227',
     gray_700: '#AAAAAA',
-
+    blue_800: '#0F1516',
+    blue_900: '#0B0F10',
     green_700: '#7BF4BA'
   }
-}
+};
