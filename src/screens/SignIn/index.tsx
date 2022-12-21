b1a9b03a56d7cf1e6ecaabf5fa145556e895e7ea
marlisonmourao <bentesmourao@gmail.com>
2022-12-20 20:33:35 -0400

    tela de Login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,30 @@
-import { Container, ImageBackground } from "./styles";
+import {
+  ButtonSignin,
+  Container,
+  Inputs,
+  TextButton,
+  ContainerForm,
+  Heading,
+  Logo,
+} from "./styles";
+
+import logoSignin from "@assets/Logo.png";
 
 export function SignIn() {
   return (
     <Container>
-      
+      <Logo source={logoSignin} />
+
+      <Heading>Acesse sua conta</Heading>
+
+      <ContainerForm>
+        <Inputs placeholder="E-mail" />
+        <Inputs placeholder="Senha" />
+
+        <ButtonSignin>
+          <TextButton>Acessar</TextButton>
+        </ButtonSignin>
+      </ContainerForm>
     </Container>
-  )
+  );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,10 +1,58 @@
+import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${({ theme }) => theme.colors.gray_800};
-`
+  background-color: ${({ theme }) => theme.colors.gray_900};
+  justify-content: center;
+  align-items: center;
 
-export const ImageBackground = styled.Image` 
-  position: absolute;
-`
+  padding: 0 15px;
+`;
+
+export const Logo = styled.Image.attrs(() => ({
+  resizeMode: "contain",
+}))`
+  width: 200px;
+  height: 200px;
+`;
+
+export const Heading = styled.Text`
+  margin-top: 40px;
+  font-size: 20px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const ContainerForm = styled.View`
+  width: 100%;
+  margin-top: 16px;
+`;
+
+export const Inputs = styled.TextInput`
+  width: 100%;
+  height: 56px;
+
+  background-color: ${({ theme }) => theme.colors.gray_300};
+  color: ${({ theme }) => theme.colors.gray_500};
+  font-size: 16px;
+
+  border-radius: 6px;
+  margin-bottom: 16px;
+  padding: 16px;
+`;
+
+export const ButtonSignin = styled(TouchableOpacity)`
+  width: 100%;
+  height: 56px;
+  background-color: ${({ theme }) => theme.colors.green_700};
+  justify-content: center;
+
+  margin-top: 32px;
+  border-radius: 6px;
+`;
+export const TextButton = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 22px;
+  text-align: center;
+`;
