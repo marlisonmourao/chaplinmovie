054086a89f2ef03f47c1e30085efb0ec29d9f266
marlisonmourao <bentesmourao@gmail.com>
2023-02-03 23:51:00 -0400

    fetching data from async storage

--- src/storage/storageUser.ts
+++ src/storage/storageUser.ts
@@ -1,16 +1,15 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { UserTokenDTO } from './../dtos/tokenDTO';
 import { USER_STORAGE } from './storageConfig'
 
-export async function storageTokenSave(token: UserTokenDTO){
+export async function storageTokenSave(token: string){
   await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(token));
 }
 
 export async function storageTokenGet() {
   const storage = await AsyncStorage.getItem(USER_STORAGE)
 
-  const token: UserTokenDTO = storage ? JSON.parse(storage) : {}
+  const token = storage ? JSON.parse(storage) : {}
 
   return token
 }
